2372511b67bd8f367725dceb5ee6194ee3d08374
Mirza Rizky <mirzarizky@gmail.com>
2020-07-20 22:35:13 +0700

    better spacing nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -93,27 +93,27 @@
       <div
         v-if="isOpen"
         tabindex="0"
-        class="flex flex-col items-center min-h-screen px-3 pt-2 space-y-4 text-center focus:outline-none origintop"
+        class="flex flex-col items-center min-h-screen px-3 pt-3 space-y-3 text-center focus:outline-none origintop"
         @keydown.esc="isOpen = false"
       >
         <nuxt-link
           active-class="text-hijau"
           to="/work"
-          class="w-full py-1 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
+          class="w-full pt-1 pb-3 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
           @click.native="visit()"
           >work</nuxt-link
         >
         <nuxt-link
           active-class="text-hijau"
           to="/about"
-          class="w-full py-1 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
+          class="w-full pt-1 pb-3 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
           @click.native="visit()"
           >about</nuxt-link
         >
         <nuxt-link
           active-class="text-hijau"
           to="/contact"
-          class="w-full py-1 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
+          class="w-full pt-1 pb-3 transition duration-300 ease-in-out border-b border-gray-300 hover:text-hijau focus:outline-none focus:text-hijau"
           @click.native="visit()"
           >contact</nuxt-link
         >
